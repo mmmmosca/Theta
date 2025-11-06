84a29c2e094fd1a121cf07227bbf74395f30297b
peonynopes <peonynopes@gmail.com>
2025-11-06 16:54:46 +0100

    More expression types.

--- bin/parser.ml
+++ bin/parser.ml
@@ -1,4 +1,12 @@
-type expression = Imp of string | Call of string * expression list
+type expression =
+  | Imp of string
+  | Let of string * expression
+  | Block of expression list
+  | Access of expression * string
+  | Call of expression * expression list
+  | Id of string
+  | Str of string
+  | FuncDef of string * string list * expression
 
 let rec parse = function
   | [] -> []
